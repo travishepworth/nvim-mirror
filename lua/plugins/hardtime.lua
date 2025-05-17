1b25d93e610e47c51d2355936b6ca888f59d8987
travmonkey <travis@batk.me>
2025-05-17 15:23:02 -0600

    Disable hardtime

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,6 +1,7 @@
-return {
-   "m4xshen/hardtime.nvim",
-   lazy = false,
-   dependencies = { "MunifTanjim/nui.nvim" },
-   opts = {},
-}
+-- return {
+--    "m4xshen/hardtime.nvim",
+--    lazy = false,
+--    dependencies = { "MunifTanjim/nui.nvim" },
+--    opts = {},
+-- }
+return {}
