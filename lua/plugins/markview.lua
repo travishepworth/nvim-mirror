a2c635bf01ab17cb2319941041793686d61dc9dd
travmonkey <t.coding@batk.me>
2025-06-16 01:17:09 -0600

    Back to markview

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,14 +1,14 @@
--- return {
---   "OXY2DEV/markview.nvim",
---   lazy = false,
---
---   config = function()
---     -- Colors for markdown viewing
---     require("markview").setup({
---       preview = {
---         icon_provider = "devicons", -- Use nvim-web-devicons for icons
---       },
---     })
---   end,
--- };
-return {}
+return {
+  "OXY2DEV/markview.nvim",
+  lazy = false,
+
+  config = function()
+    -- Colors for markdown viewing
+    require("markview").setup({
+      preview = {
+        icon_provider = "devicons", -- Use nvim-web-devicons for icons
+      },
+    })
+  end,
+};
+-- return {}
